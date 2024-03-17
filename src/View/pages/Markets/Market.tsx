82d1cd541df6b7741e28baabb77e75c6f64f6bf2
Mootassam <mootassame@gmail.com>
2024-03-17 13:08:41 +0400

    add the new commit

--- src/View/pages/Markets/Market.tsx
+++ src/View/pages/Markets/Market.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 function Market() {
