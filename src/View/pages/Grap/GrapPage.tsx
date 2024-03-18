fd83a82d314cdc7c3578f3bf0d5ec58b917838fb
Mootassam <mootassame@gmail.com>
2024-03-18 16:16:01 +0400

    add the grappage desing

--- src/View/pages/Grap/GrapPage.tsx
+++ src/View/pages/Grap/GrapPage.tsx
@@ -4,7 +4,34 @@ function Grappage() {
   return <div className="app__grappage">
 
 
-    <div className="grap__order"></div>
+    <div className="grap__order">
+
+      <div>
+
+        <div className="first__order">
+          <div className="vip__title">Shopee</div>
+          <div>
+            <label className="vip__commission">Commission Rate:</label>
+            <label className="comission"> 0.26%</label>
+          </div>
+        </div>
+        <div className="second__order">
+          <div>
+            <span className="exclusive__channel">Exclusive channel for exclsuive members</span>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className="grap__slots"></div>
+
+      <div>
+        <span className="exclusive__chaneels">Exclusive channel for exclsuive members
+        </span>
+      </div>
+
+    </div>
 
     <div className="button__grap">
       <div className="button__upgrade">
@@ -17,7 +44,7 @@ function Grappage() {
 
     <div className="order__comission">
       <div className="today__achievements">
-        <div>Today's achievements</div>
+        <div className="comission__title">Today's achievements</div>
         <div className="achivemnts__refreshe">
           <i className="fa-solid fa-rotate-right click"></i>
         </div>
