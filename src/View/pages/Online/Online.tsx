8f34c45c031ee420d28db55bdd0018f0ff486496
Mootassam <mootassame@gmail.com>
2024-03-18 18:10:27 +0400

    add the customer online photo

--- src/View/pages/Online/Online.tsx
+++ src/View/pages/Online/Online.tsx
@@ -20,7 +20,9 @@ function Online() {
 
         {Array.from({ length: 5 }).map(() => <div className="contact__online">
           <div className="list__header"> WhatsApp online </div>
-          <div className="online__image"> image of the whatsApp</div>
+          <div className="online__image">
+            <img src="/images/online/customer.jpg" alt="" />
+          </div>
           <div className="online__footer"> Number phone of whatsApp</div>
         </div>)}
 
