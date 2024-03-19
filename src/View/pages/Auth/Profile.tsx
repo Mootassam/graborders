c6ecc7057462a8ddb2b07b96c5c77d1fc52ee95b
Mootassam <mootassame@gmail.com>
2024-03-19 09:51:16 +0400

    add the styling about the invitation

--- src/View/pages/Auth/Profile.tsx
+++ src/View/pages/Auth/Profile.tsx
@@ -5,14 +5,27 @@ function Profile() {
     <div className="app__profile">
 
       <div className="profile__header">
-        <div className="profile__vip"></div>
+        <div className="profile__vip">
+          <img src="https://img.freepik.com/premium-photo/profile-shot-asian-cute-woman-perfect-skin-turn-left-smiling-joyfully-pose-near-blue-background-standing-queue-awaiting-coffee-take-away-relaxed-chatting-girlfriend-wear-yellow-summer-t-shirt_1258-57885.jpg" alt="" className="profile__photo" />
+        </div>
         <div className="profile__user">
-          <div> Javedd </div>
-          <div> invitation code : teku9a</div>
+          <div className="user__header">
+            <div className="user__name">Javedd</div>
+            <div className="user__vip"> <img src="/images/profile/diamond.png" alt="vip__profile" className="diamond__vip" /> Lv1 </div>
+          </div>
+          <div> invitation code :
+            <span className="user__name">
+              teku9a </span></div>
         </div>
       </div>
 
-      <div className="profile__subheader"></div>
+      <div className="profile__subheader">
+
+<div className="subheader__balance">
+  Available balance
+</div>
+        
+      </div>
       <div className="profile__content">
         <div className="profile__link">
           <div><i className="fa-solid fa-clock-rotate-left profile__icon" ></i></div>
