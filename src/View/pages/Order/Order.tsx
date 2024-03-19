70b29e1f17cc6ef24d3b8f05e151d4ad0e8b06a5
Mootassam <mootassame@gmail.com>
2024-03-19 14:28:22 +0400

    add the list of the orders with the deffertnts status

--- src/View/pages/Order/Order.tsx
+++ src/View/pages/Order/Order.tsx
@@ -1,5 +1,187 @@
+import { useState } from "react";
 
 function Portfolio() {
+
+  const [active, setActive] = useState("all");
+
+  const ListPending = () => {
+    return <>
+
+      {Array.from(({ length: 5 })).map(() => <div className="single__product">
+        <div className="order__time">
+          <div>Order Time: 2022-06-27 14:03:52</div>
+          <div>Order Number: N02206271321321354165</div>
+        </div>
+        <div className="badge__ pending">
+          <label>Pending</label></div>
+        <div className="product__image">
+          <div className="image__"></div>
+          <div className="product__detail">
+            <div className="detail__name">Van Hueusen Men's Slim Fir Never Tuck Short Sleeve Button Down Shirt</div>
+            <div className="detail__price">
+              <div> 100.82</div>
+              <div>X 1</div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="bottom__cadre">
+          <div className="cadre__detail">
+            <div>Total order amount</div>
+            <div>100.82</div>
+          </div>
+          <div className="cadre__detail">
+            <div>Commission</div>
+            <div>0.26</div>
+          </div>
+          <div className="cadre__detail">
+            <div>Estimated return</div>
+            <div>101.08</div>
+          </div>
+        </div>
+      </div>)}
+
+    </>
+
+  }
+
+  const Completed = () => {
+    return <>
+      {Array.from(({ length: 5 })).map(() => <div className="single__product">
+        <div className="order__time">
+          <div>Order Time: 2022-06-27 14:03:52</div>
+          <div>Order Number: N02206271321321354165</div>
+        </div>
+        <div className="badge__ completed">
+          <label>Completed</label></div>
+        <div className="product__image">
+
+          <div className="image__"></div>
+
+          <div className="product__detail">
+            <div className="detail__name">Van Hueusen Men's Slim Fir Never Tuck Short Sleeve Button Down Shirt</div>
+            <div className="detail__price">
+              <div> 100.82</div>
+              <div>X 1</div>
+            </div>
+          </div>
+
+        </div>
+
+
+        <div className="bottom__cadre">
+
+          <div className="cadre__detail">
+            <div>Total order amount</div>
+            <div>100.82</div>
+          </div>
+
+          <div className="cadre__detail">
+            <div>Commission</div>
+            <div>0.26</div>
+          </div>
+
+          <div className="cadre__detail">
+            <div>Estimated return</div>
+            <div>101.08</div>
+          </div>
+        </div>
+      </div>)}</>
+  }
+
+  const Canceled = () => {
+    return <>
+
+      {Array.from(({ length: 5 })).map(() => <div className="single__product">
+        <div className="order__time">
+          <div>Order Time: 2022-06-27 14:03:52</div>
+          <div>Order Number: N02206271321321354165</div>
+        </div>
+        <div className="badge__ canceled">
+          <label>Canceled</label></div>
+        <div className="product__image">
+
+          <div className="image__"></div>
+
+          <div className="product__detail">
+            <div className="detail__name">Van Hueusen Men's Slim Fir Never Tuck Short Sleeve Button Down Shirt</div>
+            <div className="detail__price">
+              <div> 100.82</div>
+              <div>X 1</div>
+            </div>
+          </div>
+
+        </div>
+
+
+        <div className="bottom__cadre">
+
+          <div className="cadre__detail">
+            <div>Total order amount</div>
+            <div>100.82</div>
+          </div>
+
+          <div className="cadre__detail">
+            <div>Commission</div>
+            <div>0.26</div>
+          </div>
+
+          <div className="cadre__detail">
+            <div>Estimated return</div>
+            <div>101.08</div>
+          </div>
+        </div>
+      </div>)}
+
+    </>
+
+  }
+
+  const All = () => {
+
+    return <>
+      {Array.from(({ length: 5 })).map(() => <div className="single__product">
+        <div className="order__time">
+          <div>Order Time: 2022-06-27 14:03:52</div>
+          <div>Order Number: N02206271321321354165</div>
+        </div>
+        <div className="badge__ completed">
+          <label>Completed</label></div>
+        <div className="product__image">
+
+          <div className="image__"></div>
+
+          <div className="product__detail">
+            <div className="detail__name">Van Hueusen Men's Slim Fir Never Tuck Short Sleeve Button Down Shirt</div>
+            <div className="detail__price">
+              <div> 100.82</div>
+              <div>X 1</div>
+            </div>
+          </div>
+
+        </div>
+
+
+        <div className="bottom__cadre">
+
+          <div className="cadre__detail">
+            <div>Total order amount</div>
+            <div>100.82</div>
+          </div>
+
+          <div className="cadre__detail">
+            <div>Commission</div>
+            <div>0.26</div>
+          </div>
+
+          <div className="cadre__detail">
+            <div>Estimated return</div>
+            <div>101.08</div>
+          </div>
+        </div>
+      </div>)}</>
+  }
 
   return (
     <div>
@@ -27,62 +209,21 @@ function Portfolio() {
         <div className="order__list">
 
           <div className="list__actions">
-            <div className="active__order">
+            <div className={active === 'all' ? `active__order` : ''} onClick={() => setActive("all")}>
               <span className="">All</span>
             </div>
-            <div>             <span>Pending</span></div>
-            <div>             <span>Completed</span></div>
-            <div>             <span>Canceled</span></div>
+            <div onClick={() => setActive("pending")} className={active === 'pending' ? `active__order` : ''} >             <span>Pending</span></div>
+            <div onClick={() => setActive("completed")} className={active === 'completed' ? `active__order` : ''} >             <span>Completed</span></div>
+            <div onClick={() => setActive("canceled")} className={active === 'canceled' ? `active__order` : ''} >             <span>Canceled</span></div>
           </div>
         </div>
 
         <div className="list__product">
-
-          {Array.from(({ length: 5 })).map(() => <div className="single__product">
-            <div className="order__time">
-              <div>Order Time: 2022-06-27 14:03:52</div>
-              <div>Order Number: N02206271321321354165</div>
-            </div>
-            <div className="badge__ completed">
-              <label>Completed</label></div>
-            <div className="product__image">
-
-              <div className="image__"></div>
-
-              <div className="product__detail">
-                <div className="detail__name">Van Hueusen Men's Slim Fir Never Tuck Short Sleeve Button Down Shirt</div>
-                <div className="detail__price">
-                  <div> 100.82</div>
-                  <div>X 1</div>
-                </div>
-              </div>
-
-            </div>
-
-
-            <div className="bottom__cadre">
-
-              <div className="cadre__detail">
-                <div>Total order amount</div>
-                <div>100.82</div>
-              </div>
-
-              <div className="cadre__detail">
-                <div>Commission</div>
-                <div>0.26</div>
-              </div>
-
-              <div className="cadre__detail">
-                <div>Estimated return</div>
-                <div>101.08</div>
-              </div>
-            </div>
-          </div>)}
-
+          {active === 'all' ? <All /> : ''}
+          {active === 'completed' ? <Completed /> : ''}
+          {active === 'pending' ? <ListPending /> : ""}
+          {active === 'canceled' ? <Canceled /> : ""}
         </div>
-
-
-
 
         <div className="no__moredata">
           <span>no more data</span>
