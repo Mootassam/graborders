a0cdacacee41daa6271a5191b193abe91d9fc9ff
Mootassam <mootassame@gmail.com>
2024-03-23 17:34:00 +0400

    add transaction list

--- src/View/pages/Transactions/Transaction.tsx
+++ src/View/pages/Transactions/Transaction.tsx
@@ -4,6 +4,40 @@ import SubHeader from "../../../View/shared/Header/SubHeader";
 function Transaction() {
   const [active, setActive] = useState("all");
 
+  const all = () => {
+    return (
+      <div className="content__transaction">
+        <div className="transaction__left">
+          <div>Order amount return</div>
+          <div className="transaction__date">2024-06-24 15:17</div>
+        </div>
+        <div className="transaction__right">100.82</div>
+      </div>
+    );
+  };
+  const withdraw = () => {
+    return (
+      <div className="content__transaction">
+        <div className="transaction__left">
+          <div>Order amount return</div>
+          <div className="transaction__date">2024-06-24 15:17</div>
+        </div>
+        <div className="transaction__right">100.82</div>
+      </div>
+    );
+  };
+  const deposit = () => {
+    return (
+      <div className="content__transaction">
+        <div className="transaction__left">
+          <div>Order amount return</div>
+          <div className="transaction__date">2024-06-24 15:17</div>
+        </div>
+        <div className="transaction__right red">-0.82</div>
+      </div>
+    );
+  };
+
   return (
     <div>
       <SubHeader title="Transaction" path="/profile" />
@@ -29,6 +63,14 @@ function Transaction() {
             <span>Deposit</span>
           </div>
         </div>
+      </div>
+
+      <div className="transaction__content">
+        {active === "all" && Array.from({ length: 14 }).map((item) => all())}
+        {active === "withdraw" &&
+          Array.from({ length: 7 }).map((item) => withdraw())}
+        {active === "deposit" &&
+          Array.from({ length: 6 }).map((item) => deposit())}
       </div>
     </div>
   );
