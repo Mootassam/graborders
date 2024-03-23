6e02b6f0888b0c0db4e2e57fa46437ab0c530643
Mootassam <Mootassame@gmail.com>
2024-03-23 16:38:48 +0100

    add the item

--- src/View/layout/TabBottomNavigator.tsx
+++ src/View/layout/TabBottomNavigator.tsx
@@ -47,7 +47,7 @@ function TabBottomNavigator() {
         >
           <div className="singleTab">
             <i
-              style={{ fontSize: 24 }}
+              style={{ fontSize: 21 }}
               className={`${item.icon} ${isActive(item.path) && "active"}`}
             ></i>
             <p className={`text__link ${isActive(item.path) && "active"}`}>
