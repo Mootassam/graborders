1684275edd0dd1a6ca1658728aa73c699fbf2a4a
Mootassam <mootassame@gmail.com>
2024-03-23 13:55:00 +0400

    add this information and the buttons hnad the address about the wallet page

--- src/View/pages/wallet/Wallet.tsx
+++ src/View/pages/wallet/Wallet.tsx
@@ -5,7 +5,28 @@ function Wallet() {
   return (
     <div>
       <SubHeader title="Wallet" path="/profile" />
-      <h1>Wallet</h1>
+      <div className="app__wallet">
+        <div className="wallet__">
+          <h3 className="hall">Withdrawal method information</h3>
+          <div className="wallet__form">
+            <div className="form__group">
+              <div className="label__form">
+                <span style={{ color: "red" }}>*</span>
+                <span>USDT-TRC20</span>
+              </div>
+              <div className="input__div">
+                <input
+                  type="text"
+                  name=""
+                  placeholder="Please enter network address"
+                  id=""
+                   className="input__"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
