78e90e8892d90e81c027f6ee5e33b4150375d1a9
Mootassam <mootassame@gmail.com>
2024-03-23 14:10:54 +0400

    add the change password screen

--- src/View/pages/Auth/ChangePassword.tsx
+++ src/View/pages/Auth/ChangePassword.tsx
@@ -5,8 +5,65 @@ function ChangePassword() {
   return (
     <div>
       <SubHeader title="Change password" path="/profile" />
+      <div className="app__wallet">
+        <div className="wallet__">
+          <h3 className="hall">Change Password</h3>
+          <div className="wallet__form">
+            <div className="form__">
+              <div className="form__group">
+                <div className="label__form">
+                  <span style={{ color: "red" }}>*</span>
+                  <span style={{ fontSize: "13px" }}>Old Password</span>
+                </div>
+                <div className="input__div">
+                  <input
+                    type="text"
+                    name=""
+                    placeholder="Please enter network address"
+                    id=""
+                    className="input__"
+                  />
+                </div>
+              </div>
+              <div className="form__group">
+                <div className="label__form">
+                  <span style={{ color: "red" }}>*</span>
+                  <span style={{ fontSize: "13px" }}>New Password</span>
+                </div>
+                <div className="input__div">
+                  <input
+                    type="text"
+                    name=""
+                    placeholder="Please enter network address"
+                    id=""
+                    className="input__"
+                  />
+                </div>
+              </div>
+              <div className="form__group">
+                <div className="label__form">
+                  <span style={{ color: "red" }}>*</span>
+                  <span style={{ fontSize: "13px" }}>Confirm Password</span>
+                </div>
+                <div className="input__div">
+                  <input
+                    type="text"
+                    name=""
+                    placeholder="Please enter network address"
+                    id=""
+                    className="input__"
+                  />
+                </div>
+              </div>
+            </div>
 
-      <h1>Change password</h1>
+            <div className="button__submit">Submit</div>
+            <span style={{ fontSize: 13 }}>
+              <b>Note:</b> &nbsp; Please fill out this information carefully
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
