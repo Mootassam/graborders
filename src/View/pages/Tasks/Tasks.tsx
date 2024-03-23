46d0bd2b55c92bd16f1a49c26491d7b340841c5b
Mootassam <mootassame@gmail.com>
2024-03-23 14:20:21 +0400

    add the small modification

--- src/View/pages/Tasks/Tasks.tsx
+++ src/View/pages/Tasks/Tasks.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import SubHeader from "../../../View/shared/Header/SubHeader";
 
 export default function Tasks() {
   return (
     <div>
+              <SubHeader title="Tasks" path="/profile" />
+
       <h1>Tasks</h1>
     </div>
   );
