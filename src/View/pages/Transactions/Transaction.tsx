98232dd531cab34b759f9d5b6c6f76fb2552db9b
Mootassam <mootassame@gmail.com>
2024-03-23 14:29:19 +0400

    add the list of the munue about the transaction

--- src/View/pages/Transactions/Transaction.tsx
+++ src/View/pages/Transactions/Transaction.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import SubHeader from "../../../View/shared/Header/SubHeader";
 
 function Transaction() {
+  const [active, setActive] = useState("all");
+
   return (
     <div>
       <SubHeader title="Transaction" path="/profile" />
-      <h1>Transaction</h1>
+      <div className="order__list">
+        <div className="list__transaction">
+          <div
+            className={active === "all" ? `active__transacttion` : ""}
+            onClick={() => setActive("all")}
+          >
+            <span className="">All</span>
+          </div>
+          <div
+            onClick={() => setActive("withdraw")}
+            className={active === "withdraw" ? `active__transacttion` : ""}
+          >
+            {" "}
+            <span>Withdraw</span>
+          </div>
+          <div
+            onClick={() => setActive("deposit")}
+            className={active === "deposit" ? `active__transacttion` : ""}
+          >
+            <span>Deposit</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
