7aaa2136c1c1f90d62005d9f7120789cc6821933
Mootassam <mootassame@gmail.com>
2024-03-23 13:57:56 +0400

    add the new modifications

--- src/View/pages/wallet/Wallet.tsx
+++ src/View/pages/wallet/Wallet.tsx
@@ -9,21 +9,44 @@ function Wallet() {
         <div className="wallet__">
           <h3 className="hall">Withdrawal method information</h3>
           <div className="wallet__form">
-            <div className="form__group">
-              <div className="label__form">
-                <span style={{ color: "red" }}>*</span>
-                <span>USDT-TRC20</span>
+            <div className="form__">
+              <div className="form__group">
+                <div className="label__form">
+                  <span style={{ color: "red" }}>*</span>
+                  <span style={{ fontSize: "13px" }}>USDT-TRC20</span>
+                </div>
+                <div className="input__div">
+                  <input
+                    type="text"
+                    name=""
+                    placeholder="Please enter network address"
+                    id=""
+                    className="input__"
+                  />
+                </div>
               </div>
-              <div className="input__div">
-                <input
-                  type="text"
-                  name=""
-                  placeholder="Please enter network address"
-                  id=""
-                   className="input__"
-                />
+              <div className="form__group">
+                <div className="label__form">
+                  <span style={{ color: "red" }}>*</span>
+                  <span style={{ fontSize: "13px" }}>USDT-ERC20</span>
+                </div>
+                <div className="input__div">
+                  <input
+                    type="text"
+                    name=""
+                    placeholder="Please enter network address"
+                    id=""
+                    className="input__"
+                  />
+                </div>
               </div>
             </div>
+
+            <div className="button__submit">Submit</div>
+            <span style={{ fontSize: 13 }}>
+              <b>Note:</b> &nbsp; this informations not modified so please make
+              sure that you fill it correctly
+            </span>
           </div>
         </div>
       </div>
